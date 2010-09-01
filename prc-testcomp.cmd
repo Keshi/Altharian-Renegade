51b5cf5c037ac6bdc314accf8c584a1367467b4b
Keshi <Carson@.(none)>
2010-08-31 21:09:31 -0700

    prc compiler

---
+++ prc-testcomp.cmd
@@ -0,0 +1,2 @@
+nwnnsscomp -cego -i "C:\Games\neverwinter Nights\PRC-src\include" %1.nss
+del %1.ncs
---
+++ run prc compiler.cmd
@@ -0,0 +1,3 @@
+"C:\Games\Neverwinter Nights\utils\nwnnsscomp.exe" -cego -i "C:\Games\Neverwinter Nights\PRC-src\include" *.nss
+del *.ncs
+PAUSE
